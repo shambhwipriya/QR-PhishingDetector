48197d672e454df27cc2ef957e79ddd8d4662816
shambhwi <priyashambhwi0@gmail.com>
2026-08-01 01:50:03 +0530

    Prepare backend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ import joblib
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from feature import extract_features
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -86,5 +87,7 @@ def check():
         }), 500
 
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
